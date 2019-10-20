2efeb713c763f4cbc5569bd378453bdba1779d92
ch3rrydrunk <serkhar@gmail.com>
2019-10-20 19:17:09 +0300

    Board + Game changed

--- Game.class.php
+++ Game.class.php
@@ -5,11 +5,18 @@ class Game
 	private $_board;
 	private $_player1;
 	private $_player2;
+	private $_gameStatus = -1;
 
-	public function __construct()
+	public function __construct($kwargs)
 	{
-        $this->_id = md5(random_bytes(32));
-        $this->_board = new Board();
+	    if isset($kwargs['login1'], $kwargs['login2'], $kwargs['faction'], $kwargs['fleet_set'])
+        {
+            $this->_id = md5(random_bytes(32));
+            $this->_gameStatus = 0;
+            $this->_board = new Board();
+            $this->_player1 = new Player($kwargs['login1'], $kwargs['faction'], $kwargs['fleet_set'], "up");
+            $this->_player2 = new Player($kwargs['login1'], $kwargs['faction'], $kwargs['fleet_set'], "down");
+        }
 
 	}
 }
