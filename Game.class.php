531ecd04e3ff769329c86c12ad579f1406ce9aca
ch3rrydrunk <serkhar@gmail.com>
2019-10-20 17:17:54 +0300

    - session file added to keep track of session - Game.class work in progress

--- Game.class.php
+++ Game.class.php
@@ -1,11 +1,14 @@
 <?php
 class Game
 {
+    private $_id;
 	private $_gameField;
-	private $_players;
+	private $_player1;
+	private $_player2;
 
 	public function __construct()
 	{
+        $this->_id = md5(random_bytes(32));
 
 	}
 }
